ec9a922a7991614fb647d5f21316ba87a78c071f
geekysteph <steventfchiu@gmail.com>
2023-11-28 13:01:05 -0800

    adding changes

--- basic.sh
+++ basic.sh
@@ -4,4 +4,4 @@ cd ~/
 ls -al
 touch my_command_history.txt
 history >> my_command_history.txt
-scp my_command_history.txt parallels@10.211.55.7:/~tmp
+scp my_command_history.txt parallels@10.211.55.7:/tmp
